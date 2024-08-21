f86db3d2604e1e3a59d463793a5c8bbd68b7c542
arianeBC <arianebrosseaucote@gmail.com>
2024-08-21 14:17:31 +1100

    Add User, Role, and Token entities with repositories

--- bookhub/src/main/java/com/example/bookhub/BookhubApplication.java
+++ bookhub/src/main/java/com/example/bookhub/BookhubApplication.java
@@ -2,12 +2,14 @@ package com.example.bookhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BookhubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookhubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookhubApplication.class, args);
+    }
 
 }
---
+++ bookhub/src/main/java/com/example/bookhub/user/Token.java
@@ -0,0 +1,31 @@
+package com.example.bookhub.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String tokenValue;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
