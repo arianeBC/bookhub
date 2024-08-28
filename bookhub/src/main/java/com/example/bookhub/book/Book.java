24bc9688b55cc9ccd95e4bb6e6624eed0620ff69
arianeBC <arianebrosseaucote@gmail.com>
2024-08-28 22:07:57 +1100

    Add entities for Book and Feedback

---
+++ bookhub/src/main/java/com/example/bookhub/book/Book.java
@@ -0,0 +1,28 @@
+package com.example.bookhub.book;
+
+import com.example.bookhub.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    private String title;
+    private String author;
+    private String isbn;
+    private String description;
+    private LocalDate publishedDate;
+    private boolean available;
+    private boolean archived;
+}
---
+++ bookhub/src/main/java/com/example/bookhub/feedback/Feedback.java
@@ -0,0 +1,21 @@
+package com.example.bookhub.feedback;
+
+import com.example.bookhub.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback extends BaseEntity {
+
+    private Double rating;
+    private String comment;
+}
