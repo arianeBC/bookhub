79830d25038973ca4e425834784e499753cf31f6
arianeBC <arianebrosseaucote@gmail.com>
2024-09-04 19:46:49 +1100

    Initialization of Angular project with openapi

---
+++ bookhub-ui/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from "@angular/core";
+import {AppComponent} from "./app.component";
+import {BrowserModule, provideClientHydration} from "@angular/platform-browser";
+import {AppRoutingModule} from "./app-routing.module";
+import {provideHttpClient} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    provideClientHydration(),
+    provideHttpClient()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
