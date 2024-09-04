2a9309b4d83f0ae127b3e38c7521ff4caefde180
arianeBC <arianebrosseaucote@gmail.com>
2024-09-04 22:32:51 +1100

    Add basic login page structure

--- bookhub-ui/src/app/app.module.ts
+++ bookhub-ui/src/app/app.module.ts
@@ -2,19 +2,23 @@ import {NgModule} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {BrowserModule, provideClientHydration} from "@angular/platform-browser";
 import {AppRoutingModule} from "./app-routing.module";
-import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withFetch} from "@angular/common/http";
+import {LoginComponent} from './pages/login/login.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration(),
-    provideHttpClient()
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
---
+++ bookhub-ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,50 @@
+import {Component} from '@angular/core';
+import {AuthenticationRequest} from "../../services/models/authentication-request";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  authRequest: AuthenticationRequest = {email: '', password: ''};
+  errorMessage: Array<String> = [];
+  showPassword = false; // Track password visibility state
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {
+  }
+
+  login() {
+    this.errorMessage = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: () => {
+        // TODO save token
+        this.router.navigate(['books'])
+      },
+      error: (err) => {
+        console.log(err);
+        if (err.error.validationsErrors) {
+          this.errorMessage = err.error.validationsErrors;
+        } else {
+          this.errorMessage.push(err.error.error);
+        }
+      }
+    })
+  }
+
+  register() {
+    this.router.navigate(['register']);
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword; // Toggle the state
+  }
+}
