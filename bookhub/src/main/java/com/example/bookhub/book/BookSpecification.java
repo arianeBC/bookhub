aad20f01053ef94ed9e7ea011918565ca64a7248
arianeBC <arianebrosseaucote@gmail.com>
2024-09-10 13:35:56 +1100

    refactor: Fix typo, fix HTML closing tags, reorganize SCSS

--- bookhub/src/main/java/com/example/bookhub/book/BookSpecification.java
+++ bookhub/src/main/java/com/example/bookhub/book/BookSpecification.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.domain.Specification;
 public class BookSpecification {
 
     public static Specification<Book> withOwnerId(Long ownerId) {
-        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("ownerId").get("id"), ownerId);
+        return (root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("owner").get("id"), ownerId);
     }
 }
