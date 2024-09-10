2926ae1941dc895aa766c96e99b9efe912c5e86e
arianeBC <arianebrosseaucote@gmail.com>
2024-09-10 13:36:34 +1100

    feat: Add functionality to create new books

--- bookhub-ui/src/app/modules/book/book.module.ts
+++ bookhub-ui/src/app/modules/book/book.module.ts
@@ -6,7 +6,10 @@ import {MainComponent} from './pages/main/main.component';
 import {MenuComponent} from './components/menu/menu.component';
 import {BookListComponent} from './pages/book-list/book-list.component';
 import {BookCardComponent} from './components/book-card/book-card.component';
-import { RatingComponent } from './components/rating/rating.component';
+import {RatingComponent} from './components/rating/rating.component';
+import {MyBooksComponent} from './pages/my-books/my-books.component';
+import {ManageBookComponent} from './pages/manage-book/manage-book.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -15,12 +18,15 @@ import { RatingComponent } from './components/rating/rating.component';
     MenuComponent,
     BookListComponent,
     BookCardComponent,
-    RatingComponent
+    RatingComponent,
+    MyBooksComponent,
+    ManageBookComponent
   ],
   imports: [
     CommonModule,
     BookRoutingModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    FormsModule
   ]
 })
 export class BookModule {
---
+++ bookhub-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -0,0 +1,79 @@
+import {Component, OnInit} from '@angular/core';
+import {BookRequest} from "../../../../services/models/book-request";
+import {BookService} from "../../../../services/services/book.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-manage-book',
+  templateUrl: './manage-book.component.html',
+  styleUrl: './manage-book.component.scss'
+})
+export class ManageBookComponent implements OnInit {
+  bookRequest: BookRequest = {author: "", description: "", isbn: "", title: ""}
+  errorMessage: Array<string> = [];
+  selectedFile: any;
+  selectedCoverImage: string | undefined;
+
+  constructor(
+    private bookService: BookService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+  }
+
+  ngOnInit(): void {
+    const bookId = this.activatedRoute.snapshot.params['bookId'];
+    if (bookId) {
+      this.bookService.getBook({
+        'book-id': bookId
+      }).subscribe({
+        next: (book) => {
+          this.bookRequest = {
+            id: book.id,
+            title: book.title as string,
+            author: book.author as string,
+            isbn: book.isbn as string,
+            description: book.description as string,
+            available: book.available
+          };
+          this.selectedCoverImage = 'data:image/jpg;base64,' + book.coverImage;
+        }
+      });
+    }
+  }
+
+  saveBook() {
+    this.bookService.savedBook({
+      body: this.bookRequest
+    }).subscribe({
+      next: (bookId) => {
+        this.bookService.uploadBookCoverImage({
+          'book-id': bookId,
+          body: {
+            sourceFile: this.selectedFile
+          }
+        }).subscribe({
+          next: () => {
+            this.router.navigate(['/books/my-books']);
+          }
+        });
+      },
+      error: (err) => {
+        console.log(err);
+        this.errorMessage = err.error.validationsErrors;
+      }
+    });
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    console.log(this.selectedFile);
+    if (this.selectedFile) {
+      const reader = new FileReader;
+      reader.onload = () => {
+        this.selectedCoverImage = reader.result as string;
+      }
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+}
